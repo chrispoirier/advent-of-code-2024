f564aae6a5d6d1bfba7e361ff515a5885c7c3c1b
Chris Poirier <chris.poirier@gmail.com>
2024-12-09 22:00:17 -0700

    D1P1

---
+++ common/fileio.py
@@ -0,0 +1,4 @@
+def read_input(file_name):
+    with open(file_name, "r") as input:
+        data = input.readlines()
+    return data
---
+++ day1/part1/part1.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('../../')
+from common.fileio import read_input
+from list import extract_numbers
+
+#data = read_input("test.txt")
+data = read_input("input.txt")
+first = extract_numbers(data, 0)
+second = extract_numbers(data, 1)
+first.sort()
+second.sort()
+
+diff = 0
+for i in range(len(first)):
+    diff += abs(first[i] - second[i])
+
+print(diff)
